a4562e84794af4f4aff8ff6c3ce7d3d746bbbe23
Kunal Lade <86625941+Kunalcodingdev@users.noreply.github.com>
2021-10-03 14:54:01 +0000

    Add a user input for countdown date
    Take Input from user so that they can set their own date for which they want to start a countdown for

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-let newYears = '1 January 2022'
+let newYears = document.getElementById("DateForCountdown");
 let Days = document.getElementById("days");
 let Hours = document.getElementById("hours");
 let Minutes = document.getElementById("min");
 let Seconds = document.getElementById("sec");
 
 let Countdown = () => {
-    const newYearsDate = new Date(newYears);
+    const newYearsDate = new Date(newYears.value);
     const currentDate = new Date();
 
     const totalseconds = Math.floor((newYearsDate - currentDate) / 1000);
@@ -14,10 +14,18 @@ let Countdown = () => {
     const hours = Math.floor(totalseconds / 3600) % 24;
     const days = Math.floor(totalseconds/ 3600 / 24);
 
-    Days.textContent = days;
-    Hours.textContent = hours;
-    Minutes.textContent = minutes;
-    Seconds.textContent = seconds;
+    Days.textContent = TimeAfterTen(days);
+    Hours.textContent = TimeAfterTen(hours);
+    Minutes.textContent = TimeAfterTen(minutes);
+    Seconds.textContent = TimeAfterTen(seconds);
+}
+
+let TimeAfterTen = (time) =>{
+    if (time < 10) {
+        return `0${time}`;
+    }else{
+        return time;
+    }
 }
 Countdown();
 setInterval(Countdown, 1000)
